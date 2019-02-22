a2d56bf9dfc758bee4a6b13489d71c48da350179
David Beck <david.beck@savvyapps.com>
2019-02-21 19:56:12 -0800

    Integrate api and client

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+const path = require('path');
 const express = require("express");
 const { postgraphile } = require("postgraphile");
 const PgSimplifyInflectorPlugin = require("@graphile-contrib/pg-simplify-inflector");
 
 const app = express();
+
 app.use(
   postgraphile(process.env.DATABASE_URL || "postgres://localhost/todo?sslmode=disable", "app", {
     dynamicJson: true,
@@ -12,4 +14,13 @@ app.use(
     watchPg: process.env.NODE_ENV !== "production",
   })
 );
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
 app.listen(process.env.PORT || 44593);
